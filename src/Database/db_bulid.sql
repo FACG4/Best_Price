7142d0550cc1b901ed39f6f0bbd148b97f7f9bb1
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-05-21 09:41:33 +0300

    edit tables

--- src/Database/db_bulid.sql
+++ src/Database/db_bulid.sql
@@ -1,34 +1,39 @@
 BEGIN;
 DROP TABLE IF EXISTS users, items,views,saved CASCADE;
+
+
  CREATE TABLE users(
-   id serial PRIMARY KEY,
-   user_name varchar UNIQUE NOT NULL,
+   id SERIAL PRIMARY KEY,
+   user_name VARCHAR(60) UNIQUE NOT NULL,
    first_name VARCHAR(50) NOT NULL,
    last_name VARCHAR(50) NOT NULL,
    password VARCHAR NOT NULL CHECK(char_length(password)>8),
    email VARCHAR(50) UNIQUE NOT NULL
  );
+
+
 CREATE TABLE items (
-	id serial PRIMARY KEY,
-	name varchar(30) NOT NULL,
-	shop_name varchar(30) NOT NULL,
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
+	shop_name VARCHAR(30) NOT NULL,
 	size varchar(10) NOT NULL,
 	price FLOAT NOT NULL,
-	location varchar(50) NOT NULL,
-	gender varchar(20) NOT NULL,
-	class  varchar(20) NOT NULL,
-	img  varchar NOT NULL,
-  views1 varchar(400) NOT NULL,
-	views2 varchar(400) NOT NULL,
-	views3 varchar(400) NOT NULL,
-	views4 varchar(400) NOT NULL
+	location VARCHAR(50) NOT NULL,
+	gender VARCHAR(10) NOT NULL,
+	class VARCHAR(20) NOT NULL,
+	img VARCHAR NOT NULL,
+  views1 VARCHAR NOT NULL,
+	views2 VARCHAR NOT NULL,
+	views3 VARCHAR NOT NULL,
+	views4 VARCHAR NOT NULL
 );
 
 
 CREATE TABLE saved (
-	id serial PRIMARY KEY,
+	id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   items_id INTEGER REFERENCES items(id) ON DELETE CASCADE
 );
+
 
 COMMIT;
