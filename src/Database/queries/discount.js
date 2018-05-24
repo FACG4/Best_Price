d4fe4ea4b50af3ebf7185d5c67dd3b17b5ed38b8
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-05-24 13:01:46 +0300

    fixed query

--- src/Database/queries/discount.js
+++ src/Database/queries/discount.js
@@ -1,7 +1,7 @@
 const connection = require('../db_connection');
 const disconutValue = (req,cb) =>{
   const sql = {
-    text:"SELECT img FROM items INNER JOIN disconut on items.id=disconut.items_id ",
+    text:"SELECT items.img,items.id FROM items INNER JOIN disconut on items.id=disconut.items_id ",
   }
   connection.query(sql,(dbConnectionError,result)=>{
     if (dbConnectionError) return cb(dbConnectionError)
