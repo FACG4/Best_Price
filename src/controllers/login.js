71660658184378fdd1df0a52913481f5f0ed81d9
ishak <ishak-3@hotmail.com>
2018-05-30 14:08:28 +0300

    saved edits

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -11,35 +11,27 @@ exports.get = (req, res) => {
   }
 };
 
-
 exports.post = (req, res) => {
+const { username, password } = req.body;
 
-  const {username, password } = req.body;
-  getuserData(username, (err, data) => {
-    if (data.length===0) {
-      res.send({ error: true, message: 'Wrong username or password' });
-    } else {
-      bcrypt.compare(password, data[0].password, (err, response) => {
-        if (err) throw new Error(err, 'compare');
-        if (response === false) {
-          res.send({ error: true, message: 'Wrong username or password11' });
-        } else {
+getuserData(username, (err, data) => {
+if (data.length === 0) {
+res.send({ error: true, message: 'Wrong username or password' });
+} else {
+bcrypt.compare(password, data[0].password, (err, response) => {
+if (err) throw new Error(err, 'compare');
+if (response === false) {
+res.send({ error: true, message: 'Wrong username or password11' });
+} else {
+const secret = process.env.key;
+const token = jwt.sign({ userId: data[0].id, userName: data[0].user_name }, secret);
 
-          const secret = process.env.key;
-          const token = jwt.sign({ userId: data[0].id, userName: data[0].user_name }, secret);
-        
-          res.cookie('session', token, { maxAge: 1000000, httpOnly: true });
-          req.user = data[0].user_name
-          res.send({msg:'success', user:req.user})
-          const key = process.env.key;
-          const token = jwt.sign({ userId: data[0].id, userName: data[0].user_name }, key);
-          console.log(token,'token');
-          res.cookie('cookie', token, { maxAge: 1000000, httpOnly: true });
-            
-          res.redirect('/');
+      res.cookie('cookie', token, { maxAge: 1000000, httpOnly: true });
+      req.user = data[0].user_name;
 
-        }
-      });
+      res.send({ msg: 'success', user: req.user });
     }
   });
+}
+});
 };
